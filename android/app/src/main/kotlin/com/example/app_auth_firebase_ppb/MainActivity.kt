69be7528a19e79769d485c4d76036f4188c33e93
mikungg <mikhael.sappy@gmail.com>
2025-05-14 16:51:09 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/app_auth_firebase_ppb/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.app_auth_firebase_ppb
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:app_auth_firebase_ppb/screens/home.dart';
+import 'package:app_auth_firebase_ppb/screens/login.dart';
+import 'package:app_auth_firebase_ppb/screens/register.dart';
+import 'package:app_auth_firebase_ppb/screens/second_screen.dart';
+import 'package:app_auth_firebase_ppb/sevices/notification_service.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await NotificationService.initializeNotification();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  static GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: 'login',
+      routes: {
+        'home': (context) => const HomeScreen(),
+        'second': (context) => const SecondScreen(),
+        'login': (context) => const LoginScreen(),
+        'register': (context) => const RegisterScreen(),
+      },
+      navigatorKey: navigatorKey,
+    );
+  }
+}
